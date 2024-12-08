0c2b24f0c9c3a7400ba652f4bdeee869c8f7c367
Kauan Felipe de Moura <kauanmoura04082004@gmail.com>
2024-12-08 06:30:47 -0300

    add manual

--- algoritmos/busca_largura.py
+++ algoritmos/busca_largura.py
@@ -15,7 +15,6 @@ class Largura:
         self.goal = goal
         self.pai = {self.start: (0, 0)}
 
-
     def search(self):
         """
         Algoritmo Busca em largura
@@ -29,32 +28,32 @@ class Largura:
 
             # Se o nó não foi visitado
             if node not in self.visited:
-                self.visited.add(node) # Marca o nó como visitado
-                self.path.append(node) # Adiciona o nó ao caminho
+                self.visited.add(node)  # Marca o nó como visitado
+                self.path.append(node)  # Adiciona o nó ao caminho
 
                 # Verifica se os vizinhos do nó
                 for neighbor in self.graph.neighbors(node):
 
-                    if neighbor not in self.pai: # Se o vizinho não tem pai
-                        self.pai[neighbor] = (node, 0) # Armazena o pai e o custo
+                    if neighbor not in self.pai:  # Se o vizinho não tem pai
+                        self.pai[neighbor] = (node, 0)  # Armazena o pai e o custo
 
-                    if neighbor not in self.visited: # Se o vizinho não foi visitado
-                        self.queue.append(neighbor) # Adiciona o vizinho a fila
+                    if neighbor not in self.visited:  # Se o vizinho não foi visitado
+                        self.queue.append(neighbor)  # Adiciona o vizinho a fila
 
     def best_path(self):
         """
         Extrai o melhor caminho
         :return: melhor caminho
         """
-        path_best = [] # Caminho final
-        no_atual = self.goal # Nó atual é o objetivo
+        path_best = []  # Caminho final
+        no_atual = self.goal  # Nó atual é o objetivo
 
         # Reconstrua o caminho a partir do objetivo, seguindo os pais
         while no_atual != self.start:
-            path_best.append(no_atual) # Adiciona o nó atual ao caminho
+            path_best.append(no_atual)  # Adiciona o nó atual ao caminho
             no_atual = self.pai[no_atual][0]  # Pega o pai do nó atual
 
-        path_best.append(self.start) # Adiciona o nó inicial ao caminho
-        path_best.reverse() # Inverte o caminho
+        path_best.append(self.start)  # Adiciona o nó inicial ao caminho
+        path_best.reverse()  # Inverte o caminho
 
         return path_best
